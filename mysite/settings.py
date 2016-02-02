bc3f816358ed314fa245ebfdabb43662e899c48b
tarundev92 <tarundev92@yahoo.com>
2016-02-02 18:19:40 +0530

    Second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'tests.apps.TestsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ tests/tests.py
@@ -0,0 +1,91 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PollsTest(LiveServerTestCase):
+
+    fixtures = ['admin_user.json']
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_create_new_poll_via_admin_site(self):
+        # Gertrude opens her web browser, and goes to the admin page
+        self.browser.get(self.live_server_url + '/admin/')
+
+        # She sees the familiar 'Django administration' heading
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+
+        # She types in her username and passwords and hits return
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('mysite123')
+        password_field.send_keys(Keys.RETURN)
+
+        # her username and password are accepted, and she is taken to
+        # the Site Administration page
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+
+        # She now sees a couple of hyperlink that says "Polls"
+        polls_links = self.browser.find_elements_by_link_text('Questions')
+        self.assertEquals(len(polls_links), 1)
+        
+        # The second one looks more exciting, so she clicks it
+        polls_links[0].click()
+
+        # She is taken to the polls listing page, which shows she has
+        # no polls yet
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('0 questions', body.text)
+
+        # She sees a link to 'add' a new poll, so she clicks it
+        new_poll_link = self.browser.find_element_by_link_text('Add question')
+        new_poll_link.click()
+        
+        # She sees some input fields for "Question" and "Date published"
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Question text:', body.text)
+        self.assertIn('Date published:', body.text)
+
+
+        # She types in an interesting question for the Poll
+        question_field = self.browser.find_element_by_name('question_text')
+        question_field.send_keys("How awesome is Test-Driven Development?")
+
+        # She sets the date and time of publication - it'll be a new year's
+        # poll!
+        date_field = self.browser.find_element_by_name('pub_date_0')
+        date_field.send_keys('01/01/12')
+        time_field = self.browser.find_element_by_name('pub_date_1')
+        time_field.send_keys('00:00')
+
+        
+        # She sees she can enter choices for the Poll.  She adds three
+        choice_1 = self.browser.find_element_by_name('choice_set-0-choice_text')
+        choice_1.send_keys('Very awesome')
+        choice_2 = self.browser.find_element_by_name('choice_set-1-choice_text')
+        choice_2.send_keys('Quite awesome')
+        choice_3 = self.browser.find_element_by_name('choice_set-2-choice_text')
+        choice_3.send_keys('Moderately awesome')
+
+        # Gertrude clicks the save button
+        save_button = self.browser.find_element_by_xpath("//input[@name='_save'][@value='Save']")
+        save_button.click()
+
+        # She is returned to the "Polls" listing, where she can see her
+        # new poll, listed as a clickable link
+        new_poll_links = self.browser.find_elements_by_link_text(
+            "How awesome is Test-Driven Development?"
+    )
+        self.assertEquals(len(new_poll_links), 1)
+
+        # Satisfied, she goes back to sleep
+
+
