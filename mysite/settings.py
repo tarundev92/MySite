742f63bfe309f09ceef01aa6979f3addf3d5f7a4
tarundev92 <tarundev92@yahoo.com>
2016-02-02 19:23:24 +0530

    6th commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
-    'tests.apps.TestsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
+    'tests',
 ]
 
 MIDDLEWARE_CLASSES = [
