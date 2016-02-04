ba62d3153cad887b6e9d8b3f255566a787d0c9d1
tarundev <tarundev92@yahoo.com>
2016-02-04 13:16:53 +0530

    7th commit

---
+++ deploy_prod.sh
@@ -0,0 +1,14 @@
+ssh dev@188.166.187.156
+TextN00k
+sudo bash
+TextN00k
+source env/bin/activate
+cd MySiteRepo
+git clone https://github.com/tarundev92/MySite.git
+cd MySite
+cd mysite
+sed -i 's/'"'"'PASSWORD'"'"''":"' '"'"''"'"''","'/'"'"'PASSWORD'"'"''":"' '"'"'root'"'"''","'/g' settings.py
+cd ..
+python manage.py makemigrations
+python manage.py migrate
+python manage.py test polls
