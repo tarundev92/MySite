d4cf177198d710cda76e3c981947c2b114c5ac08
tarundev92 <tarundev92@yahoo.com>
2016-02-08 12:49:02 +0530

    Update deploy_staging.sh

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -5,4 +5,4 @@ rm -rfv ./.git
 rm -rfv ./venv
 find . -name \*.pyc -type f -delete
 find . -name \*.*~ -type f -delete
-scp -r . root@188.166.187.156:/home/dev/temp/$dir_name
+scp -r . root@188.166.187.156:/home/dev/temp/basename "$PWD"
