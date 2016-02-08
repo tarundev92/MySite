8d76f27f32422677df37824712d326d5dd176248
tarundev92 <tarundev92@yahoo.com>
2016-02-08 12:25:41 +0530

    Update deploy_staging.sh

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -2,13 +2,7 @@ pwd
 ls
 dir_name=basename "$PWD"
 ssh root@188.166.187.156
-if [ ! -d /home/dev/temp/$dir_name  ] 
-then
-   mkdir /home/dev/temp/$dir_name
-else
-cd /home/dev/temp/$dir_name
-rm -rfv ./*
-fi
+[ ! -d /home/dev/temp/$dir_name  ] && mkdir /home/dev/temp/$dir_name || cd /home/dev/temp/$dir_name && rm -rfv ./*
 exit
 rm -rfv ./.git
 find . -name \*.pyc -type f -delete
