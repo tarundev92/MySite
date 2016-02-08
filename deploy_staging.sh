e4199dc468eb0ab8510367efb4195278970a3dc2
tarundev92 <tarundev92@yahoo.com>
2016-02-08 10:50:18 +0530

    Update deploy_staging.sh

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,11 +1,6 @@
 pwd
-git config --global user.email "tarundev92@yahoo.com"
-git config --global user.name "tarundev92"
-git init
-git add -all
-git commit -m "1st"
-git remote add origin https://github.com/tarundev92/Deploy-MySite.git
-git pull
-git push -u origin master
-tarundev92
-Dev8904644994
+ls
+cd ..
+pwd
+ls
+scp -r temp root@188.166.187.156:/home/dev/temp
