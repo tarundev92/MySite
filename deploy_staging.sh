714ac95246ac1750ba6518609d8feba0640213a7
tarundev92 <tarundev92@yahoo.com>
2016-02-08 12:52:20 +0530

    Update deploy_staging.sh

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -6,4 +6,4 @@ rm -rfv ./venv
 find . -name \*.pyc -type f -delete
 find . -name \*.*~ -type f -delete
 basename "$PWD"
-scp -r . root@188.166.187.156:/home/dev/temp/basename "$PWD"
+scp -r . root@188.166.187.156:/home/dev/temp/"basename "$PWD""
