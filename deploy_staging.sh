c34cf7567e336a3a4eeee9db06afaa157ebd29d9
tarundev92 <tarundev92@yahoo.com>
2016-02-08 12:42:58 +0530

    Update deploy_staging.sh

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -2,6 +2,7 @@ pwd
 ls
 dir_name=basename "$PWD"
 rm -rfv ./.git
+rm -rfv ./venv
 find . -name \*.pyc -type f -delete
 find . -name \*.*~ -type f -delete
 scp -r . root@188.166.187.156:/home/dev/temp/$dir_name
