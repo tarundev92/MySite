9b9282b216d96586635edf78fa919bbc856a01b7
tarundev92 <tarundev92@yahoo.com>
2016-02-08 12:22:23 +0530

    Update deploy_staging.sh

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,6 +1,16 @@
 pwd
 ls
-cd ..
-pwd
-ls
-scp -r MySite root@188.166.187.156:/home/dev/temp
+dir_name=basename "$PWD"
+ssh root@188.166.187.156
+if [ ! -d /home/dev/temp/$dir_name  ] 
+then
+   mkdir /home/dev/temp/$dir_name
+else
+cd /home/dev/temp/$dir_name
+rm -rfv ./*
+fi
+exit
+rm -rfv ./.git
+find . -name \*.pyc -type f -delete
+find . -name \*.*~ -type f -delete
+scp -r . root@188.166.187.156:/home/dev/temp/$dir_name
