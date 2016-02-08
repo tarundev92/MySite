a07db30de1e67e393510b03d07dc8ebbc72d6a51
tarundev92 <tarundev92@yahoo.com>
2016-02-08 14:27:50 +0530

    Update deploy_staging.sh

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,9 +1,8 @@
-pwd
-ls
-dir_name = basename "$PWD"
+#!/bin/bash
+cdir=$(basename "$PWD")
+echo $cdir
+ssh root@188.166.187.156 /home/dev/temp/script.sh $cdir
 rm -rfv ./.git
-rm -rfv ./venv
 find . -name \*.pyc -type f -delete
 find . -name \*.*~ -type f -delete
-basename "$PWD"
-scp -r . root@188.166.187.156:/home/dev/temp/"basename "$PWD""
+scp -r . root@188.166.187.156:/home/dev/temp/$cdir
