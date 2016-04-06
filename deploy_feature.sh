aacc963f740591abc0f415190defb1f84ce43db3
tarundev92 <tarundev92@yahoo.com>
2016-04-06 16:42:14 +0530

    wsgi fix

--- deploy_feature.sh
+++ deploy_feature.sh
@@ -9,3 +9,19 @@ find . -name \*.*~ -type f -delete
 scp -r . root@188.166.187.156:/home/dev/temp/feature/$cdir
 ssh root@188.166.187.156 'bash -s' < script1.sh /home/dev/temp/feature
 
+
+source /opt/myenv/bin/activate
+export PYTHONPATH=/home/tarun/projects/MysiteGitRepo/MySite
+export DJANGO_SETTINGS_MODULE=mysite.settings
+export HOST_NAME=http://127.0.01/
+/home/tarun/projects/MysiteGitRepo/myenv/bin/gunicorn -c /home/tarun/projects/MysiteGitRepo/MySite/scripts/gunicorn.py mysite.wsgi --reload
+
+/home/tarun/projects/MysiteGitRepo/myenv/bin/gunicorn  mysite.wsgi:application -b 127.0.0.1:8000
+
+source /var/www/appzcentre.com/djenv/bin/activate
+export PYTHONPATH=/var/www/appzcentre.com/MySite
+export DJANGO_SETTINGS_MODULE=mysite.settings
+export HOST_NAME=http://45.55.185.30/
+python manage.py runserver 45.55.185.30:8000
+/var/www/appzcentre.com/djenv/bin/gunicorn -c /var/www/appzcentre.com/MySite/gunicorn.py mysite.wsgi --reload
+
---
+++ scripts/gunicorn.py~
@@ -0,0 +1,5 @@
+command = '/var/www/appzcentre.com/djenv/bin/gunicorn'
+pythonpath = '/var/www/appzcentre.com/MySite'
+bind = '127.0.0.1:8001'
+timeout='30000000'
+
